5ffd9e68e0acd4159fa27835a2f690879f34d236
Dave Trollope <daveanddiane@kringlecottage.com>
2017-02-21 13:37:19 -0600

    add repo and author views

--- app/controllers/pull_request_controller.rb
+++ app/controllers/pull_request_controller.rb
@@ -2,6 +2,8 @@ class PullRequestController < ApplicationController
   require 'hash_arrays'
 
   def open
+    params_to_session
+
     file = GithubDataFile.most_recent('archive', '*_open_pr_data.json')
 
     file_data = GithubDataFile.load_files(file)
@@ -12,25 +14,37 @@ class PullRequestController < ApplicationController
   end
 
   def closed
+    params_to_session
+
     file = GithubDataFile.most_recent('archive', '*_closed_pr_data.json')
 
     file_data = GithubDataFile.load_files(file)
 
     pr_data = file_data.present? ? file_data.last[:pr_data].where(state: 'closed') : []
 
-    pr_data = pr_data.where(merged_at: /./) if filter_enabled?(:unmerged, false) == false
+    pr_data = pr_data.where(merged_at: /./) if filter_value?(:unmerged, false) == false
 
-    render locals: { pr_data: pr_data }
+    render "_closed_#{session['view_type']}", locals: { pr_data: pr_data }
+  end
+
+  def filter_syms
+    [:unmerged, :view_type]
+  end
+
+  def params_to_session
+    filter_syms.each {|sym|
+      session[sym.to_s] = params[sym] if params[sym].present?
+    }
   end
 
   def set_filters
-    [:unmerged].each {|sym|
+    filter_syms.each {|sym|
       session[sym.to_s] = params[sym] || false
     }
     redirect_back fallback_location: root_path
   end
 
-  def filter_enabled?(sym, default_value = true)
+  def filter_value?(sym, default_value = true)
     session.include?(sym.to_s) ? session[sym.to_s] : default_value
   end
 end
--- app/helpers/pull_request_helper.rb
+++ app/helpers/pull_request_helper.rb
@@ -2,4 +2,8 @@ module PullRequestHelper
   def self.github_pr_path(repo, id)
     "#{AppConfig.github.httpserver}/#{repo}/pull/#{id}"
   end
+
+  def self.github_repo_path(repo)
+    "#{AppConfig.github.httpserver}/#{repo}"
+  end
 end
