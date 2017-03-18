7e13957356ab064491b2d40b04b391857f5aa014
Dave Trollope <daveanddiane@kringlecottage.com>
2017-03-18 10:16:21 -0500

    add a bunch of projects to schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -24,29 +24,78 @@
 # Open PRs
 every :hour, at: 0 do
   runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_hour, 'ruby', state: 'open'"
-  # runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_hour, 'jmmastey', state: 'open'"
 end
 
-every :hour, at: 15 do
+every :hour, at: 5 do
   runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_hour, 'rails', state: 'open'"
 end
 
-every :hour, at: 30 do
+every :hour, at: 10 do
   runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_hour, 'python', state: 'open'"
 end
 
-# Closed PRs
-every 1.day, at: '5:00 am' do
-  # runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_today"
-  runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_today, 'ruby'"
-  # runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_today, 'jsor', closed_days: 60"
-  # runner "require 'github_data_file'; GithubDataFile.get_org_prs 'archive', GithubDataFile.prefix_today, 'enova'"
+every :hour, at: 15 do
+  runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_hour, 'golang', state: 'open'"
 end
 
-every 1.day, at: '6:00 am' do
+every :hour, at: 20 do
+  runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_hour, 'elixir-lang', state: 'open'"
+end
+
+every :hour, at: 25 do
+  runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_hour, 'clojure', state: 'open'"
+end
+
+every :hour, at: 30 do
+  runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_hour, 'jruby', state: 'open'"
+end
+
+every :hour, at: 35 do
+  runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_hour, 'graalvm', state: 'open'"
+end
+
+every :hour, at: 40 do
+  runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_hour, 'erlang', state: 'open'"
+end
+
+# Closed PRs
+# runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_today"
+# runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_today, 'jsor', closed_days: 60"
+# runner "require 'github_data_file'; GithubDataFile.get_org_prs 'archive', GithubDataFile.prefix_today, 'enova'"
+
+every :day, at: '3:02 am' do
+  runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_today, 'golang'"
+end
+
+every :day, at: '4:02 am' do
+  runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_today, 'elixir-lang'"
+end
+
+every :day, at: '4:32 am' do
+  runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_today, 'erlang'"
+end
+
+every :day, at: '5:02 am' do
+  runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_today, 'ruby'"
+end
+
+every :day, at: '5:32 am' do
+  runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_today, 'jruby'"
+end
+
+every :day, at: '6:02 am' do
   runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_today, 'rails'"
 end
 
-every 1.day, at: '7:00 am' do
+every :day, at: '6:32 am' do
+  runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_today, 'graalvm'"
+end
+
+every :day, at: '7:02 am' do
   runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_today, 'python'"
 end
+
+every :day, at: '8:02 am' do
+  runner "require 'github_data_file'; GithubDataFile.get_user_prs 'archive', GithubDataFile.prefix_today, 'clojure'"
+end
+
