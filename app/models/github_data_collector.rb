c1815687632df9f7715058d7912c4cef267d1950
Dave Trollope <daveanddiane@kringlecottage.com>
2017-03-29 21:31:53 -0500

    resolve rebase conflicts on logging and concurrency settings

--- app/models/github_data_collector.rb
+++ app/models/github_data_collector.rb
@@ -18,45 +18,100 @@ class GithubDataCollector
 
     require 'net/http'
 
-    uri = URI("#{AppConfig.github.server}/repos/#{repo}/pulls")
-
     response_data = []
-    page = 0
-    pages = 1
 
     if constraint.count > 0
       limit_field = constraint.first[0]
       limit_time = Time.now - constraint.first[1]
     end
 
+    all_responses = []
+    exceptions = []
+
+    pool = Thread.pool(AppConfig.github_data_collector.thread_pool.pr_pool_size)
+    merge_mutex = Mutex.new
+    exception_mutex = Mutex.new
+
+    pages, response_json = fetch_pullrequest_page(constraint, limit_field, limit_time, 0, 1, repo, state)
+    response_data << response_json
+
+    page = 1
+    batch_size = 25
+
+    while page < pages
+      pool.process(page) {|lpage|
+        begin
+          lpages, response_json = fetch_pullrequest_page(constraint, limit_field, limit_time,
+                                                         lpage, [ batch_size, pages - lpage ].min, repo, state)
+
+          merge_mutex.synchronize {
+            response_data << response_json
+          }
+        rescue StandardError, WebMock::NetConnectNotAllowedError => e
+          Rails.logger.error "#{e.message} #{e.backtrace}"
+          exception_mutex.synchronize {
+            exceptions << e
+          }
+        end
+      }
+      page += batch_size
+    end
+
+    pool.shutdown
+
+    exceptions.each {|e| raise e }
+
+    response_data.flatten!
+  end
+
+  def fetch_pullrequest_page(constraint, limit_field, limit_time, page, num_pages, repo, state)
+    pages = 1
+    last_page = page + num_pages
+    response_data = []
+
+    uri = URI("#{AppConfig.github.server}/repos/#{repo}/pulls")
+
     Net::HTTP.start(uri.host, uri.port,
                     use_ssl: uri.scheme == 'https',
                     verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
+
       begin
         uri = URI("#{AppConfig.github.server}/repos/#{repo}/pulls?state=#{state}&per_page=100&page=#{page + 1}")
 
-        Rails.logger.debug "Getting #{uri.inspect}"
+        response = nil
+        begin
+          retries ||= 0
+          Rails.logger.debug "Getting #{uri.inspect}"
 
-        request = Net::HTTP::Get.new uri.request_uri
-        request.basic_auth GithubDataCollector.username, ENV['GITHUB_PASSWORD']
+          request = Net::HTTP::Get.new uri.request_uri
+          request.basic_auth GithubDataCollector.username, ENV['GITHUB_PASSWORD']
 
-        response = http.request request # Net::HTTPResponse object
+          response = http.request request # Net::HTTPResponse object
+          if response.code == '403' && (retries += 1) < 3
+            Rails.logger.info 'github returned 403, retrying in ~5s'
+            sleep(4 + (rand(2000) / 1000.0))
+            raise
+          end
+        rescue
+          retry
+        end
         raise GithubBadResponse.new "Bad response from github #{response.code} #{response.body}" if response.code != '200'
 
         GithubDataCollector.log_rate_limits response.header
         if response.header['link'].present?
-          page_str = response.header['link'].split(',').select {|s| s =~ /\"last\"/}.first
+          page_str = response.header['link'].split(',').select { |s| s =~ /\"last\"/ }.first
           pages = page_str[/[?&]page=[0-9]+/].scan(/[0-9]+/).first.to_i if page_str
         end
 
         response_json = JSON.parse(response.body)
-        response_json.reject! {|pr| Time.parse(pr[limit_field]) < limit_time} if constraint.count > 0
+        response_json.reject! { |pr| Time.parse(pr[limit_field]) < limit_time } if constraint.count > 0
         response_data << response_json
+
         page += 1
-      end while page < pages # rubocop:disable Lint/Loop - using a loop increases branch complexity
+      end while page < last_page
     end
 
-    response_data.flatten!
+    [pages, response_data.flatten!]
   end
 
   def fetch_pullrequests(repo, pr_data)
@@ -128,7 +183,7 @@ class GithubDataCollector
     all_prs = []
     exceptions = []
 
-    pool = Thread.pool(AppConfig.github_data_collector.thread_pool.pool_size)
+    pool = Thread.pool(AppConfig.github_data_collector.thread_pool.repo_pool_size)
     merge_mutex = Mutex.new
     exception_mutex = Mutex.new
 
@@ -156,20 +211,21 @@ class GithubDataCollector
         end
       }
     }
+
     pool.shutdown
 
     exceptions.each {|e| raise e }
     { repo_prs: all_repo_prs, pr_data: all_prs }
   end
 
-  def self.log_rate_limits(header)
+  def self.log_rate_limits(header, force = false)
     limit = header['X-RateLimit-Limit']
     remaining = header['X-RateLimit-Remaining'].to_i
     reset_time = header['X-RateLimit-Reset']
 
     if remaining.zero?
       Rails.logger.warn "RATE LIMITS #{limit}/#{remaining}/#{reset_time}"
-    elsif (remaining % 100).zero?
+    elsif force || (remaining % 100).zero?
       Rails.logger.info "RATE LIMITS #{limit}/#{remaining}/#{reset_time}"
     end
   end
